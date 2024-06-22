d3c8a34d8538cd0ea0ccf6a13c13a45e82400b4e
Joshardals <joshuabamidele219@gmail.com>
2024-06-22 17:31:30 +0100

    initial commit

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { isValidPassword } from "./lib/isValidPassword";
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(req: NextRequest) {
@@ -12,7 +13,21 @@ export async function middleware(req: NextRequest) {
 }
 
 async function isAuthenticated(req: NextRequest) {
-  return Promise.resolve(false);
+  const authHeader =
+    req.headers.get("authorization") || req.headers.get("Authorization");
+  if (authHeader == null) return false;
+
+  const [username, password] = Buffer.from(authHeader.split(" ")[1], "base64")
+    .toString()
+    .split(":");
+
+  return (
+    username === process.env.ADMIN_USERRNAME &&
+    (await isValidPassword(
+      password,
+      process.env.HASHED_ADMIN_PASSWORD as string
+    ))
+  );
 }
 
 export const config = {
