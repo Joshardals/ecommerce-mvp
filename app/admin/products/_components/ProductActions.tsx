a928e3b320f778f0079496111b5ff74a63faa399
Joshardals <joshuabamidele219@gmail.com>
2024-06-22 11:06:36 +0100

    initial commit

--- app/admin/products/_components/ProductActions.tsx
+++ app/admin/products/_components/ProductActions.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
 import { useTransition } from "react";
 import {
--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -16,8 +16,13 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import {
+  ActiveToggleDropdownItem,
+  DeleteDropdownItem,
+} from "./_components/ProductActions";
 
 export default function AdminProductPage() {
   return (
@@ -88,6 +93,15 @@ async function ProductsTable() {
                       Edit
                     </Link>
                   </DropdownMenuItem>
+                  <ActiveToggleDropdownItem
+                    id={product.id}
+                    isAvailableForPurchase={product.isAvailableForPurchase}
+                  />
+                  <DropdownMenuSeparator />
+                  <DeleteDropdownItem
+                    id={product.id}
+                    disabled={product._count.orders > 0}
+                  />
                 </DropdownMenuContent>
               </DropdownMenu>
             </TableCell>
