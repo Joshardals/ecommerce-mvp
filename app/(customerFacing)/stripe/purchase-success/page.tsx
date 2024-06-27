4c21604bf11f1fdea80f6ce7056c8a51491df082
Joshardals <joshuabamidele219@gmail.com>
2024-06-27 06:23:25 +0100

    initial commit

--- app/(customerFacing)/stripe/purchase-success/page.tsx
+++ app/(customerFacing)/stripe/purchase-success/page.tsx
@@ -2,6 +2,8 @@ import { formatCurrency } from "@/lib/formatter";
 import Image from "next/image";
 import { confirmPurchase } from "../../products/_actions/purchase.actions";
 import { notFound } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function SuccessPage({
   searchParams,
@@ -15,23 +17,29 @@ export default async function SuccessPage({
   return (
     <div className="max-w-5xl w-full mx-auto space-y-8">
       <h1 className="text-4xl">{isSuccess ? "Success" : "Error!"}</h1>
-      <h1 className=" text-4xl">{results.isSuccess ? "Success" : "Error!"}</h1>
       <div className="flex gap-4 items-center">
         <div className="aspect-video flex-shrink-0 w-1/3 relative">
           <Image
-            src={results.product.imagePath}
+            src={product.imagePath}
             fill
-            alt={results.product.name}
+            alt={product.name}
             className="object-cover"
           />
         </div>
         <div className="text-lg">
-          {formatCurrency(results.product.priceInCents / 100)}
+          {formatCurrency(product.priceInCents / 100)}
         </div>
         <h1 className="text-2xl font-bold">{results.product.name}</h1>
         <div className="line-clamp-3 text-muted-foreground">
-          {results.product.description}
+          {product.description}
         </div>
+        <Button className="mt-4" size={"lg"} asChild>
+          {isSuccess ? (
+            <a></a>
+          ) : (
+            <Link href={`/products/${product.id}/purchase`}>Try Again</Link>
+          )}
+        </Button>
       </div>
     </div>
   );
