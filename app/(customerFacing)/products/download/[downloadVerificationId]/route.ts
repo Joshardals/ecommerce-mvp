547539aa3554d8cf9bb272b51b26db148c2b8ae1
Joshardals <joshuabamidele219@gmail.com>
2024-07-01 20:54:21 +0100

    initial commit

--- app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
+++ app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
@@ -21,7 +21,7 @@ export async function GET(
   return new NextResponse(file, {
     headers: {
       "Content-Disposition": `attachment; filename="${data.product.name}.${extension}"`,
-      "Content-Length": size.toString(),
+      "Content-Length": size.toString(),  
     },
   });
 }
